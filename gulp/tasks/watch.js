c9b9cfb54d31dee6210d5cee2a28e5a832bbb76e
Artem Nikitin <a.nikitin@dataduck.com>
2022-06-23 12:34:20 +0200

    fixed scss watcher

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,10 @@
 module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('src/pug/**/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('src/static/css/**/*.scss', $.gulp.series('sass'));
+        $.gulp.watch('src/static/css/*.scss', $.gulp.series('sass'));
+        $.gulp.watch('src/static/css/common/*.scss', $.gulp.series('sass'));
+        $.gulp.watch('src/static/css/components/*.scss', $.gulp.series('sass'));
+
         $.gulp.watch(
             [
                 'src/static/js/common.js',
